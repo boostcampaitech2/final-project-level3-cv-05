5828fec4eea3a84da888472780221aeb4c9f664a
lkm2835 <lkm2835@gmail.com>
2021-12-11 12:09:56 +0000

    remove columns

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ import csv
 import base64
 
 #streamlit run app.py --server.address=127.0.0.1
-#이렇게 하면 브라우저가 Local로 띄어짐.
+#이렇게 하면 브라우저가 Local로 띄워짐.
+
 
 # Fxn
 @st.cache
 def load_image(image_file):
 	img = Image.open(image_file)
 	return img 
+
 
 #Fxn to Save answer
 def save_results(results_df,button_press,image_file,problem_name,answer):
@@ -36,6 +38,7 @@ def load_data():
 		df = pd.read_csv('answer.csv')
 	return df
 
+
 #Fxn to Save Upload csv
 def save_uploaded_csv(uploadfile):
 	if(os.path.isdir("math_data") == False): #Change path
@@ -44,6 +47,7 @@ def save_uploaded_csv(uploadfile):
 	with open(os.path.join("math_data",uploadfile.name),'wb') as f:
 		f.write(uploadfile.getbuffer())
 	return st.success("Save Answer : To Show Click Answer on Menu")
+
 
 #Fxn to Save Uploaded File to Directory
 def save_uploaded_file(uploadfile):
@@ -54,6 +58,7 @@ def save_uploaded_file(uploadfile):
 		f.write(uploadfile.getbuffer())
 	return st.success("Upload file :{} in Server".format(uploadfile.name))
 
+
 #Fxn to Save After File
 def save_after_file(file,name):
 	if(os.path.isdir("new_data")==False):
@@ -62,15 +67,18 @@ def save_after_file(file,name):
 	file.save('./new_data/{}'.format(name),'png')
 	return st.success("Upload file :{} in Server".format(name))
 
+
 #pdf 다운 (아직 완성 안됨)
 def create_download_link(val, filename):
     b64 = base64.b64encode(val)  # val looks like b'...'
     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
+
 #Object Detection
 def OD_image(image):
 	convert = image.convert("LA")
 	return convert
+
 
 #GAN
 def GAN_image(image):
@@ -81,7 +89,6 @@ def GAN_image(image):
 def streamlit_run():
 	result_df = load_data()
 	button_press = 0
-	#button_press = len(result_df)
 
 	st.title("Math wrong answer editor")
 
@@ -95,12 +102,9 @@ def streamlit_run():
 		if image_file is not None:
 			#Get Before Image
 			img = load_image(image_file)
-			#result_df = load_data()
 
-			before, od, gan, after = st.columns(4)
-
-			before.subheader("Before")
-			before.image(img, use_column_width = True)
+			st.subheader("Before")
+			st.image(img, use_column_width = True)
 
 			od_img = OD_image(img)
 			gan_img = GAN_image(img)
@@ -109,35 +113,23 @@ def streamlit_run():
 			flag_od = st.checkbox("Object Detection")
 			flag_gan = st.checkbox("GAN")
 
-			od.subheader("Only Crop")
-			gan.subheader("Only Erase")
-			after.subheader("Both")
-
-			if flag_od and flag_gan:
-				after.image(after_img,use_column_width = True)
 			if flag_od:
-				od.image(od_img,use_column_width = True)
+				st.subheader("Object Detection")
+				st.image(od_img,use_column_width = True)
 			if flag_gan:
-				gan.image(gan_img,use_column_width = True)
+				st.subheader("GAN")
+				st.image(gan_img,use_column_width = True)
+			if flag_od and flag_gan:
+				st.subheader("AFTER")
+				st.image(after_img,use_column_width = True)
 
 			st.write(image_file.name)
 
-			#write name
-			#problem_name = st.text_input('Write problem name')
-
-			#write anwer
-			#answer = st.text_input('Write answer')
-
 			#saving file
 			if st.button("Save"):
-				#button_press += 1
-				#st.write(button_press)
-				# save_results(result_df, button_press, image_file, problem_name, answer)
 				save_uploaded_file(image_file)
 				save_after_file(after_img,image_file.name)
-			#Done?
-			#if st.button("Are you done?"):
-				#save_uploaded_csv(result_df)
+
 	elif choice == "MakePDF":
 		#PDF 구현
 		report_image = st.text_input("Report Text")
@@ -154,21 +146,14 @@ def streamlit_run():
 
 			st.markdown(html, unsafe_allow_html = True)
 
-
-		
 		#Save pdf
 	elif choice == "Answer":
 		st.text("Show")
-		# st.subheader("Input name to find answer")
-
-		# df = pd.read_csv("answer.csv")
-		# st.write(df)
 	else:
 		st.subheader("About")
 		st.text("수학 오답 노트 편집기")
 		st.text("친구구했조")
 		st.text("Freinds")
-
 
 
 if __name__ == '__main__':
